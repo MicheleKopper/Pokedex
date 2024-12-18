16403efacdbd063b23367464db78fa3fb3f916ec
Michele Kopper <michelekopper@gmail.com>
2024-12-18 12:22:36 -0300

    Menu icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,38 +1,93 @@
-import logo from "../../assets/logo.png";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Fade, Menu, MenuItem } from "@mui/material";
+import React from "react";
 
 export function Navbar() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box
+      sx={{
+        flexGrow: 1,
+      }}
+    >
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar
+          sx={{
+            bgcolor: "#FFCB04",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            position: "relative",
+          }}
+        >
+          {/* MENU */}
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleClick}
           >
             <MenuIcon />
           </IconButton>
 
+          <Menu
+            id="fade-menu"
+            MenuListProps={{
+              "aria-labelledby": "menu-icon",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+            slotProps={{
+              paper: {
+                sx: {
+                  width: "200px", 
+                  bgcolor: "#f5f5f5", 
+                  padding: "8px", 
+                },
+              },
+            }}
+          >
+            <MenuItem onClick={handleClose}>Home</MenuItem>
+            <MenuItem onClick={handleClose}>Pokedex</MenuItem>
+            <MenuItem onClick={handleClose}>Contato</MenuItem>
+          </Menu>
+
           {/* LOGO */}
           <Box
-            component="img"
-            src={logo}
-            alt="Logo"
             sx={{
-              height: 40,
-              width: "auto",
-              mr: 2,
+              position: "absolute",
+              left: "50%",
+              transform: "translateX(-50%)",
             }}
-          />
+          >
+            <img
+              src="/logo.png"
+              alt="Logo"
+              style={{
+                width: "90px",
+                height: "auto",
+                objectFit: "contain",
+              }}
+            />
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
--- src/configs/global/GlobalStyled.tsx
+++ src/configs/global/GlobalStyled.tsx
@@ -1,8 +1,17 @@
 import { CssBaseline, CSSObject, GlobalStyles } from "@mui/material";
+import "@fontsource/dm-sans/100.css";
+import "@fontsource/dm-sans/200.css";
+import "@fontsource/dm-sans/300.css";
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/600.css";
+import "@fontsource/dm-sans/700.css";
+import "@fontsource/dm-sans/800.css";
+import "@fontsource/dm-sans/900.css";
 
 const styles: Record<string, CSSObject> = {
   body: {
-    fontFamily: "",
+    fontFamily: "DM Sans",
   },
 };
 
